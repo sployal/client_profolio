9327041426957f197e5c86fc746603c11a8f4aea
Davie <muigaid91@gmail.com>
2026-03-03 09:13:46 +0300

    added icon image

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="https://res.cloudinary.com/dacpiss4b/image/upload/v1772514964/main_image_wjyuuq.jpg" type="image/jpeg" />
+      <link rel="apple-touch-icon" href="https://res.cloudinary.com/dacpiss4b/image/upload/v1772514964/main_image_wjyuuq.jpg" />
+    </>
+  );
+}
